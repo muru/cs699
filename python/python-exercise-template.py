60955b301f622c31575d710d3389f0023f665d06
Murukesh Mohanan <murukesh.mohanan@gmail.com>
2013-10-10 11:10:30 +0530

    Python exercise 1.

--- python/python-exercise-template.py
+++ python/python-exercise-template.py
@@ -1,2 +1,16 @@
 #!/usr/bin/python
 
+import sys
+import csv
+
+if len(sys.argv) != 2:
+	print('Usage: ' + sys.argv[0] + ' <CSVFILE>')
+	quit(-1)
+
+with open(sys.argv[1]) as csvfile:
+	csvreader = csv.reader(csvfile)
+	numCandidates = 0
+	for row in csvreader:
+		if row[0].isdigit():
+			numCandidates = numCandidates + 1
+	print(numCandidates)
